fb4410fce941c078553b7e740b2b00f5a8c1ec3e
stacksaddup <neil.ty.lin@gmail.com>
2021-12-23 23:09:22 +0800

    Finish the step 6

--- 03.3-ways-to-take-command-line-arguments/03.by-size/by-size.sh
+++ 03.3-ways-to-take-command-line-arguments/03.by-size/by-size.sh
@@ -7,7 +7,31 @@ fi
 
 file=$1
 
-repeat=4
+repeat=1
+while (( $# > 0 ));
+do
+    case "$1" in
+	--repeat)
+	    shift
+
+	    if [[ -z $1 ]]; then
+		echo "ERROR: missing REPEAT value"
+		exit 1
+	    fi
+
+	    repeat=$1
+	    shift
+	    ;;
+	-*)
+	    echo "ERROR: unknown argument: $1"
+	    exit 1
+	    ;;
+	*)
+	    shift
+	    ;;
+    esac
+done
+
 for i in $(seq "$repeat");
 do
     n=$(( $RANDOM % 10 + 1 ))
