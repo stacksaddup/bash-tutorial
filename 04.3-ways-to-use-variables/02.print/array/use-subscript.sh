f1d892d441f7330e8931ec5b15892329bb412d41
stacksaddup <neil.ty.lin@gmail.com>
2022-01-18 21:39:46 +0800

    Move scripts into the right places

---
+++ 04.3-ways-to-use-variables/02.print/array/use-subscript.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+hosts=('192.168.56.10' '192.168.56.11' '192.168.56.12')
+echo "hosts[0]: ${hosts[0]}"
+echo "hosts[1]: ${hosts[1]}"
+echo "hosts[2]: ${hosts[2]}"
+echo "hosts[@]: ${hosts[@]}"
---
+++ 04.3-ways-to-use-variables/02.print/use-braces/print-confusing-var.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+n=5
+
+echo "Without braces:"
+echo "$nth record"
+echo
+
+echo "With braces:"
+echo "${n}th record"
